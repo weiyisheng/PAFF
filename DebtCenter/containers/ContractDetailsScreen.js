ae8254b3998f09c2e9103d24f4b478be40c073f5
xiebo <xie513195393@163.com>
2016-04-04 15:15:32 +0800

    nihao

---
+++ DebtCenter/containers/ContractDetailsScreen.js
@@ -0,0 +1,184 @@
+import React ,{
+  components ,
+  Text ,
+  ScrollView ,
+  View
+} from react-native
+
+import PAFFNavBar    from 'PAFFNavBar'
+import Cell          from './components/Cell'
+import HFStyleSheet  from 'HFStyleSheet'
+
+import { Red ,
+         Black ,
+         BorderBottom ,
+         Yellow ,
+         Yellow2
+      } from '../constants/color'
+import { ContainerNomalPadding } from '../constants/dimens'
+
+class contractDetils extends components{
+    back() {
+      this.props.navigator.pop()
+    },
+    render() {
+      return(
+        <View style = {styles.cot}>
+        <PAFFNavBar
+        title = {'合同详情'}
+        onBackPressed={() => this.back()}
+         />
+         <ScrollView style = {[styles.cot]}>
+         <Cell
+         style = {[BorderBottom]}
+         left = {{text : '贷款机构' , style : styles.lable}}
+         right = {{text : '泸州市商业银行总行' , style : styles.info}}
+         />
+         <Cell
+         left = {{text : '申请时间' , style : styles.lable}}
+         right = {{text : '2016.01.01' , style : styles.info}}
+         />
+         <Cell
+         left = {{text : '合同状态' , style : styles.lable}}
+         right = {{text : '有效' , style : styles.info}}
+         />
+         <View
+         style = {[BorderBottom , styles.detailsBox]}
+         >
+         <Cell
+         left = {{text : '授权总额' , style : styles.detailsLable}}
+         right = {{text : '10000.00' , style : styles.infoRed}}
+         />
+         <Cell
+         left = {{text : '待还款金额' , style : styles.detailsLable}}
+         right = {{text : '1000.00' , style : styles.infoRed}}
+         />
+         <Cell
+         left = {{text : '可用金额' , style : styles.detailsLable}}
+         right = {{text : '2000.00' , style : styles.infoRed}}
+         />
+         </View>
+
+         <View
+         style = {[BorderBottom , styles.detailsBox]}
+         >
+         <Cell
+         left = {{text : '质押存款' , style : styles.detailsLable}}
+         right = {{text : '24000.00' , style : styles.info}}
+         />
+         <Cell
+         right = {{text : '快乐金 2016.01.01' , style : styles.detailsInfo}}
+         />
+         </View>
+
+         <Cell
+         left = {{text : '还款方式' , style : styles.lable}}
+         right = {{text : '按月等额本息' , style : styles.info}}
+         />
+         <Cell
+         left = {{text : '还款日期' , style : styles.lable}}
+         right = {{text : '2016.01.12' , style : styles.info}}
+         />
+         <Cell
+         style = {[BorderBottom]}
+         left = {{text : '贷款机构' , style : styles.lable}}
+         right = {{text : '泸州市商业银行总行' , style : styles.info}}
+         />
+
+         <View
+         style = {[BorderBottom , styles.detailsBox]}
+         >
+         <Cell
+         left = {{text : '关联账户' , style : styles.detailsLable}}
+         />
+         <Cell
+         left = {{text : '泸州市商业银行' , style : styles.detailsLable}}
+         right = {{text : '2343 **** **** **** 999' , style : styles.detailsInfo}}
+         />
+         </View>
+         </ScrollView>
+         </View>
+      )
+  }
+}
+
+const Style = HFStyleSheet.create({
+  cot : {
+    flex : 1
+  },
+  lable : {
+    color : Black ,
+    opacity : .8 ,
+    fontSize : 25
+  },
+  info: {
+    fontSize: 15,
+    color: Black
+  },
+  infoRed: {
+    fontSize: 15,
+    color: Red
+  },
+  buttonBox: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingBottom: 20,
+    paddingTop: 10
+  },
+  button: {
+    width: 100,
+    height: 30,
+    backgroundColor: Yellow,
+    borderRadius: 70,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontSize: 13,
+    color: '#808080'
+  },
+  detailsBox: {
+    paddingHorizontal: ContainerNomalPadding
+  },
+  detailsTitleLable: {
+    marginHorizontal: 0,
+    opacity: .8,
+    color: Black,
+    fontSize: 15
+  },
+  detailsTitleInfo: {
+    marginHorizontal: 0,
+    color: Black,
+    fontSize: 15
+  },
+  detailsLable: {
+    marginHorizontal: 0,
+    marginVertical: 8,
+    opacity: .8,
+    color: Black,
+    fontSize: 14
+  },
+  detailsInfo: {
+    marginHorizontal: 0,
+    color: Black,
+    fontSize: 14,
+    marginVertical: 8,
+  },
+  totalLable: {
+    fontSize: 15,
+    color: Black
+  },
+  totalInfo: {
+    fontSize: 15,
+    color: Red
+  },
+  explain: {
+    fontSize: 15,
+    color: Black,
+    paddingHorizontal: ContainerNomalPadding,
+    marginBottom: 30
+  }
+
+})
+
+module.exports = contractDetils
---
+++ DebtCenter/containers/ContractScreen.js
@@ -0,0 +1,135 @@
+
+//components
+import React, {
+   Component,
+   View,
+   ScrollView,
+   Text,
+   ListView,
+   PixelRatio
+} from 'react-native'
+import PAFFNavBar from 'PAFFNavBar'
+import Cell from './components/Cell'
+
+import HFStyleSheet from 'HFStyleSheet'
+
+//constants
+import { Gray1, BorderColor, Yellow, Blue } from '../constants/colors'
+
+class Contract extends Component {
+
+  back() {
+    this.props.navigator.pop()
+  },
+
+  getInitialState : function(){
+        var ds = new ListView.DataSource({
+          rowHasChanged : (r1 , r2) => ri!=r2
+        });
+        return {
+          dataSource: ds.cloneWithRows(['xiebo nihao 1', 'xiebo nihao 2']),
+      };
+  },
+
+  updateDataSource : function(data){
+    this.setState({
+      dataSource : this.state.dataSource.cloneWithRows(data)
+    })
+  },
+
+  renderRow : function(){
+    return(
+      <View>
+      <View style = { styles.container}>
+      <Cell
+      left = {{text : '借据1' }}
+      right = {{text : '未结清' ， style : {color : Red}}}
+      />
+      </View>
+
+      <Cell
+      left = {{text : '2015.01.01-2016.01.01' }}
+      />
+      </View>
+      <View style = { styles.cellBorder } />
+      <View>
+      <Cell
+      left = {{text : '借据总额' }}
+      right = {{text : '10000.00'}}
+      />
+      <Cell
+      left = {{text : '待还款金额' }}
+      right = {{text : '10000.00'}}
+      />
+      <Cell
+      left = {{text : '还款方式' }}
+      right = {{text : '按月等额本金'}}
+      />
+      </View>
+    );
+  }
+  render() {
+    return (
+      <ScrollView style={[styles.cot]}>
+        <PAFFNavBar title={"合同1"} onBackPressed={() => this.back()}/>
+        <View style = { styles.container}>
+        <Cell
+        left = {{text : '授信总额'}}
+        right = {{text : '10000.00'}}
+        />
+        <Cell
+        left = {{text : '待还款金额'}}
+        right = {{text : '10000.00'}}
+        />
+        <Cell
+        left = {{text : '可用金额'}}
+        right = {{text : '10000.00'}}
+        />
+        <View style={[BorderBottom, styles.buttonBox]}>
+          <TouchableOpacity>
+            <View style={[styles.button]}>
+              <Text style={[styles.buttonText]}>逾期还款</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{marginLeft: 30}}>
+            <View style={[styles.button, {backgroundColor: Yellow2}]}>
+              <Text style={[styles.buttonText]}>提前还款</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        </View>
+        <ListView dataSource = {this.state.dataSource} renderRow = {this.renderRow} />
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+module.exports = ContractList
+
+const styles = HFStyleSheet.create({
+  cot: {
+    flex: 1
+  }
+  totalBox: {
+    backgroundColor: Gray1,
+    paddingHorizontal: ContainerNomalPadding,
+    paddingBottom: 20
+  },
+  titleBox: {
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: BorderColor,
+    alignItems: 'center'
+  },
+  title: {
+    color: Yellow,
+    fontSize: 15
+  },
+  cellBorder: {
+    backgroundColor: 'rgba(0,0,0,1)',
+    height: 1 / PixelRatio.get(),
+    marginLeft: 4
+  }
+})
